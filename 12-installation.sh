570b33362c8d6717fd2d177ab5b04b554d21e1db
sgraju <sgraju.tibco@gmail.com>
2026-02-06 09:42:07 +0400

    even or odd

---
+++ 12-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Pleae run the script with root prevelege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MYSQL is Failed"
+else
+    echo "Installing MYSQL is SUCCESS"
+fi
